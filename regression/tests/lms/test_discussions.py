7310f845bae79a890d7c9339915687ffd58b7be5
Denis <kolesnik090592@gmail.com>
2018-01-29 18:17:46 +0200

    Test for checking availability of discussions page

---
+++ regression/tests/lms/test_discussions.py
@@ -0,0 +1,32 @@
+import os
+
+from bok_choy.web_app_test import WebAppTest
+
+from regression.pages.lms import LMS_STAGE_BASE_URL, LMS_PROTOCOL
+from regression.tests.helpers.api_clients import LmsLoginApi
+from edxapp_acceptance.pages.lms.discussion import DiscussionThreadPage
+
+
+class DiscussionTest(WebAppTest):
+    """
+    Test to check that discussion page is available
+    """
+    def test_course_discussion_page(self):
+        LMS_BASE_URL = os.environ.get('LMS_BASE_URL', LMS_STAGE_BASE_URL)
+        COURSE_ORG = os.environ.get('COURSE_ORG', 'edX')
+        COURSE_NUMBER = os.environ.get('COURSE_NUMBER', 'DemoX')
+        COURSE_RUN = os.environ.get('COURSE_RUN', 'Demo_Course')
+
+        login_api = LmsLoginApi()
+        login_api.authenticate(self.browser)
+        discussion_page = DiscussionThreadPage(self.browser, '.page-content')
+
+        discussion_page.url = '{}://{}/courses/course-v1:{}+{}+{}/discussion/forum/'.format(LMS_PROTOCOL, LMS_BASE_URL,
+                            COURSE_ORG, COURSE_NUMBER, COURSE_RUN)
+
+        discussion_page.visit()
+        discussion_page.wait_for_page()
+        try:
+            discussion_page.is_browser_on_page()
+        except False:
+            print('FAIL')
