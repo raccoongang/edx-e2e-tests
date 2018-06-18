614c331f623a05137ca06637e1f50a510ea3afd7
Denis <kolesnik090592@gmail.com>
2018-06-18 12:25:49 +0300

    Fix tests for original edx

--- regression/pages/lms/course_page_lms.py
+++ regression/pages/lms/course_page_lms.py
@@ -3,6 +3,9 @@ Course info page
 """
 from edxapp_acceptance.pages.lms.course_info import CourseInfoPage
 from regression.pages.lms import LOGIN_BASE_URL
+from regression.pages.lms.utils import get_course_key
+from regression.tests.helpers.utils import (
+    get_course_info)
 
 
 class CourseInfoPageExtended(CourseInfoPage):
@@ -10,13 +13,16 @@ class CourseInfoPageExtended(CourseInfoPage):
     This class is an extended class of CourseInfoPage,
     where we add methods that are different or not used in CourseInfoPage
     """
+
     @property
     def url(self):
         """
         Construct a URL to the page within the course.
         """
+        course_info = get_course_info()
+        course_key = get_course_key(course_info)
         return "{}/courses/{}/{}".format(
-            LOGIN_BASE_URL, self.course_id, self.url_path
+            LOGIN_BASE_URL, course_key, self.url_path
         )
 
     def click_resume_button(self):
