e4fd7c0cbd8c00c988f5fef16f8a48f8c25b3de9
ruslanskira <skira.ruslan@gmail.com>
2019-03-28 14:02:34 +0200

    E2E-72 Verify a course deletion "Sysadmin"

--- regression/pages/studio/login_studio.py
+++ regression/pages/studio/login_studio.py
@@ -3,6 +3,7 @@ Studio login page
 """
 from edxapp_acceptance.pages.studio.login import LoginPage
 from regression.pages.studio import LOGIN_BASE_URL
+from bok_choy.promise import EmptyPromise
 
 
 class StudioLogin(LoginPage):
@@ -10,3 +11,23 @@ class StudioLogin(LoginPage):
     This class is an extended class of LoginPage.
     """
     url = LOGIN_BASE_URL + '/signin'
+
+    def is_browser_on_page(self):
+        return self.q(css="[id=login_form]").present
+
+    def login(self, email, password, expect_success=True):
+        """
+        Attempt to log in using 'email' and 'password'.
+        """
+        self.wait_for_element_visibility('#field-email', 'Email field is shown')
+        self.q(css="#email").fill(email)
+
+        self.q(css="#password").fill(password)
+        self.q(css="#submit").click()
+
+        # Ensure that we make it to another page
+        if expect_success:
+            EmptyPromise(
+                lambda: "login" not in self.browser.current_url,
+                "redirected from the login page"
+            ).fulfill()
