16cfbca6b5a8c696e78314c70d65bd22f6062f89
Sagirov Evgeniy <sagirov19@gmail.com>
2019-03-30 14:54:31 +0200

    fix login_studio page

--- regression/pages/studio/login_studio.py
+++ regression/pages/studio/login_studio.py
@@ -1,6 +1,8 @@
 """
 Studio login page
 """
+from time import sleep
+
 from edxapp_acceptance.pages.studio.login import LoginPage
 from regression.pages.studio import LOGIN_BASE_URL
 
@@ -10,3 +12,22 @@ class StudioLogin(LoginPage):
     This class is an extended class of LoginPage.
     """
     url = LOGIN_BASE_URL + '/signin'
+
+    def is_browser_on_page(self):
+        return self.q(css="[id=login_form]").present
+
+    def login(self, email, password):
+        """
+        Attempt to log in using 'email' and 'password'.
+        """
+        self.wait_for_element_visibility('#field-email', 'Email field is shown')
+        self.q(css="#email").fill(email)
+        self.fill_password(password)
+        sleep(1)
+        self.q(css="#submit").click()
+
+    def fill_password(self, password):
+        """
+        Fill the password field with the value.
+        """
+        self.q(css="#password").fill(password)
