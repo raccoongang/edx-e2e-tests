8df4466a36acb3bd21dc7f1bf49550e2291dfaad
ruslanskira <skira.ruslan@gmail.com>
2019-04-04 15:24:49 +0300

    E2E-73 [Data Downloads] Get student anonymized IDs CSV

--- regression/pages/lms/instructor_dashboard.py
+++ regression/pages/lms/instructor_dashboard.py
@@ -2,7 +2,8 @@
 Instructor dashboard page.
 """
 from edxapp_acceptance.pages.lms.instructor_dashboard import (
-    InstructorDashboardPage
+    InstructorDashboardPage,
+    DataDownloadPage
 )
 from regression.pages.lms import LOGIN_BASE_URL
 
@@ -11,6 +12,7 @@ class InstructorDashboardPageExtended(InstructorDashboardPage):
     """
     This class is an extended class of Instructor Dashboard Page
     """
+
     @property
     def url(self):
         """
@@ -35,3 +37,15 @@ class InstructorDashboardPageExtended(InstructorDashboardPage):
         self.q(css='p em a').click()
         self.browser.switch_to_window(self.browser.window_handles[-1])
         return self.q(css='.navbar-brand-app').text[0]
+
+
+class DataDownloadPageExtended(DataDownloadPage):
+    @property
+    def generate_student_anonymized_idscsv(self):
+        """
+        Returns the "Generate Problem Grade Report" button.
+        """
+        if self.q(css='input[name=list-anon-ids]'):
+            return self.q(css='input[name=list-anon-ids]')
+        else:
+            raise Exception('Error! Download (Get Student Anonymized IDs CSV button)  found')
